ad18633efab7ee1a590808fff28f2a9a2354a811
CELEBANTY <celestine6279@gmail.com>
2023-10-27 21:03:01 -0700

    silence

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print argument number
+ * @argc: argument count
+ * @argv: srguments vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv[0];
+
+	printf("%d\n", argc);
+	return (0);
+}
