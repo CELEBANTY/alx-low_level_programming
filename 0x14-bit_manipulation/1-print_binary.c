a5d0ec22dc5fe9506e29789f61a7376f581a2172
CELEBANTY <celestine6279@gmail.com>
2023-11-24 18:45:36 -0800

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,7 +3,7 @@
 /**
 * _pow - func calculates (base ^ power)
 * @base: base of the exponent
-* @power: base of the exponent
+* @power: power of the exponent
 *
 * Return: value of (base ^ power)
 */
@@ -14,17 +14,17 @@ unsigned long int _pow(unsigned int base, unsigned int power)
 	unsigned int a;
 
 	num = 1;
-		for  (a = 1; a <= power; a++)
-			num *= base;
+	for (a = 1; a <= power; a++)
+		num *= base;
 	return (num);
 }
 
 /**
- * print binary - prints a number in binary notation
- * @n: number to print
- *
- * Return: void
- */
+* print_binary - prints a number in binary notation
+* @n: number to print
+*
+* Return: void
+*/
 
 void print_binary(unsigned long int n)
 {
